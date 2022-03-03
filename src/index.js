4dc093d6c3ee7c5e1de08f60f4a8b0dd284f3361
GuilhermePaini <guihdpaini@gmail.com>
2022-03-02 23:43:40 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const ytdl = require('ytdl-core');
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/', (req, res) => res.send('Got a POST request'))
+
+app.post('/video', async (req, res) => {
+  const { url } = req.body;
+  const songInfo = await ytdl.getBasicInfo(url);
+  res.json(songInfo.videoDetails)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
