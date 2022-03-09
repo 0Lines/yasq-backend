bd9109c1f270025fc9360a2f2e93888a494000b3
GuilhermePaini <guihdpaini@gmail.com>
2022-03-08 23:58:31 -0300

    added cors and some env variables

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const ytdl = require('ytdl-core');
+const cors = require('cors');
+require('dotenv').config();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.HOST_PORT;
 
 app.use(express.json())
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/video', async (req, res) => {
   const { url } = req.body;
-  const songInfo = await ytdl.getBasicInfo(url);
+
+  const songInfo = await ytdl.getBasicInfo(url).catch((err) => {
+    console.log(err);
+    res.send("Error").status(500);
+  });
+
   res.json(songInfo.videoDetails);
 })
 
