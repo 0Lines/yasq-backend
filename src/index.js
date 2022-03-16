88c5a39d0bc970aa3042a50d766b28755ddeea29
Guilherme Paini <guihdpaini@gmail.com>
2022-03-16 19:31:45 -0300

    Adding npmommand to run node server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = "8081";
+const port = process.env.HOST_PORT;
 
 app.use(express.json())
 app.use(cors());
