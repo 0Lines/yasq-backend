ad5d2aa3de83c35c335ef0ab5db24934b963ba0e
Arthur <arthurbziero@gmail.com>
2022-03-31 15:41:57 -0300

    add nodemon to the project

--- src/index.js
+++ src/index.js
@@ -26,22 +26,27 @@ io.on('connection', (socket) => {
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/video', async (req, res) => {
-	const { url } = req.body;
-	const validUrl = await ytdl.validateURL(url);
+    const { url } = req.body;
+    const validUrl = await ytdl.validateURL(url);
 
 	if(!validUrl) {
-		res.status(404).send("Video not found!").end();
+		res.status(404).send('Video not found!').end();
 		return;
 	}
-	
-	const songInfo = await ytdl.getBasicInfo(url);
+
+    try {
+        const songInfo = await ytdl.getBasicInfo(url);
+    } catch(error) {
+		res.status(404).send('URL malformed or invalid!').end();
+		return;
+    }
 	res.json(songInfo.videoDetails);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
 
 server.listen(process.env.SOCKET_PORT, () => {
-	console.log(`listening on *:${process.env.SOCKET_PORT}`);
+	console.log(`Listening on ${process.env.SOCKET_PORT}`);
 });
