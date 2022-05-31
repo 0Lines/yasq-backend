05eaada457529844b752c8de8ff878d6371aa9ae
Arthur <arthurbziero@gmail.com>
2022-05-30 22:59:44 -0300

    postgres now generates the uuids

--- src/database/create_tables.sql
+++ src/database/create_tables.sql
@@ -1,19 +1,24 @@
-CREATE TABLE Users (
-  id_user VARCHAR(50) NOT NULL,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS Rooms;
+CREATE TABLE IF NOT EXISTS Rooms (
+  id_room VARCHAR(50) NOT NULL DEFAULT uuid_generate_v4(),
+  name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id_room)
+);
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE IF NOT EXISTS Users (
+  id_user VARCHAR(50) NOT NULL DEFAULT uuid_generate_v4(),
   nickname VARCHAR(50) NOT NULL,
   photo_link VARCHAR(150),
   id_room VARCHAR(50) NOT NULL REFERENCES Rooms(id_room) ON DELETE CASCADE,
   PRIMARY KEY (id_user)
 );
 
-CREATE TABLE Rooms (
-  id_room VARCHAR(50) NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id_room)
-);
-
-CREATE TABLE Songs (
-  id_music VARCHAR(50) NOT NULL,
+DROP TABLE IF EXISTS Songs;
+CREATE TABLE IF NOT EXISTS Songs (
+  id_music VARCHAR(50) NOT NULL DEFAULT uuid_generate_v4(),
   name VARCHAR(100) NOT NULL,
   artist VARCHAR(50),
   music_link VARCHAR(300) NOT NULL,
