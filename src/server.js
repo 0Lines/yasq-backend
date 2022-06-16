4e75579f0a7d297bf975d02f80b18305a797e71d
GuilhermeDaBolsa <Guilherme_Giordani@live.com>
2022-06-16 11:07:58 -0300

    organization: separating socket code from server code

--- src/server.js
+++ src/server.js
@@ -2,17 +2,12 @@ require('express-async-errors');
 const express = require ('express');
 const routes = require('./routes');
 const cors = require('cors');
-const { createServer } = require('http');
-const { Server } = require("socket.io");
-const { registerSocket } = require("./socket/socket");
+const { createSocketServer, registerSocketEvents } = require("./socket/socket");
 
 require('dotenv').config();
 
 const app = express();
-const httpPort = process.env.HTTP_PORT;
-
-const server = createServer(app);
-const io = new Server(server, { cors: { origin: "*" }});
+const httpPort = process.env.HTTP_PORT || 5000;
 
 app.use(express.static('public/resources/images'));
 app.use(cors());
@@ -23,13 +18,9 @@ app.use((error, req, res, next) => {
     res.status(error.statusCode ?? 400).send(error.message);
 });
 
-registerSocket(io);
+global.socket = createSocketServer(app);
+registerSocketEvents(global.socket);
 
 app.listen(httpPort, () => {
     console.log(`HTTP server listening on port ${httpPort}`);
 });
-
-server.listen(process.env.SOCKET_PORT, () => {
-	console.log(`Socket listening on port ${process.env.SOCKET_PORT}`);
-});
-
