e7931759a03b147cae07a103983961422770f7b8
Arthur <arthurbziero@gmail.com>
2022-07-04 22:49:36 -0300

    syncing clients via socket

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,8 @@
 const { createServer } = require('http');
 const { Server } = require("socket.io");
 
+let rooms = [];
+
 function createSocketServer(expressServer) {
 	const server = createServer(expressServer);
 	const io = new Server(server, { cors: { origin: "*" }});
@@ -15,9 +17,48 @@ function registerSocketEvents(io) {
 	io.on("connection", (socket) => {
         console.log("User connected");
 
-		socket.on("subscribeToRoom", (id_room) => socket.join(id_room));
-		socket.on("pause", (id_room) => io.to(id_room).emit("pause"));
-		socket.on("play", (id_room) => io.to(id_room).emit("play"));
+        socket.on("subscribeToRoom", (id_room) => {
+            socket.join(id_room);
+            if (rooms[id_room] == undefined)
+                rooms[id_room] = {
+                    isPlaying: false,
+                    startedAt: 0,
+                    stoppedAt: 0,
+                };
+        });
+
+		socket.on("pause", (id_room) => {
+            rooms[id_room].stoppedAt = Date.now();
+
+            rooms[id_room].isPlaying = false;
+
+            io.to(id_room).emit("pause");
+        });
+
+        socket.on("play", (id_room) => {
+            let room = rooms[id_room];
+
+            console.log(room)
+
+            const now = Date.now();
+            if (room.startedAt)
+                room.startedAt = now - ((room.isPlaying ? now : room.stoppedAt) - room.startedAt);
+            else
+                room.startedAt = now;
+
+            room.isPlaying = true;
+            io.to(id_room).emit("play", (now - room.startedAt) / 1000);
+        });
+
+        socket.on("getCurrentState", (id_room) => {
+            const room = rooms[id_room];
+            const now = Date.now();
+
+            io.to(id_room).emit("getCurrentState", {
+                isPlaying: room.isPlaying,
+                startFrom: (((room.isPlaying ? now : room.stoppedAt) - room.startedAt) / 1000),
+            });
+        });
 	});
 }
 
